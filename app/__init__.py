f6af1086ee4d8cb9693dbc1b6ee977245c6b311f
Pierre Guilbault <guilbaultpj@gmail.com>
2021-11-16 18:15:30 +0100

    transaction route working

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from datetime import datetime, time
+from flask import Flask, request
 # from flask_restful import Resource, Api, reqparse
 app = Flask(__name__)
 # api = Api(app)
@@ -12,6 +13,12 @@ TRANSACTIONS = [
         "timestamp": "2020-11-01T14:00:00Z"},
     {"payer": "DANNON", "points": 300, "timestamp": "2020-10-31T10:00:00Z"}
 ]
+
+
+# def make_balances(transactions):
+#     for transaction in transaction:
+#         if BALANCES[""]
+
 
 BALANCES = []
 
@@ -27,17 +34,26 @@ def balances():
     Returns all current balances
     """
     res = {}
-    for data_column in database:
+    for data_column in TRANSACTIONS:
         res[data_column["payer"]] = data_column["points"]
     return res
 
 
-@app.route('/transactions', methods=['POST'])
+@app.route('/transaction', methods=['POST'])
 def catch_points():
+    print("YOU ARE IN THE transaction route")
     """
     Adds points from a specific payer and at a specific time
     """
-    pass
+    data = request.get_json()
+    # return data["payer"], 201
+    payer = data["payer"]
+    points = data["points"]
+    timestamp = datetime.utcnow()
+    newTransaction = {"payer": payer, "points": points, "timestamp": timestamp}
+    TRANSACTIONS.append(newTransaction)
+    print(TRANSACTIONS)
+    return newTransaction, 201
 
 
 @app.route('/spend_points', methods=['POST'])
