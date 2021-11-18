efe0f770fbdde2abf5031010b8e37785cac62c5e
Pierre Guilbault <guilbaultpj@gmail.com>
2021-11-18 10:26:53 +0100

    adding one transaction to balances or adding points

--- sandbox.py
+++ sandbox.py
@@ -1,6 +1,5 @@
 TRANSACTIONS = [
     {"payer": "DANNON", "points": 1000, "timestamp": "2020-11-02T14:00:00Z"},
-    {"payer": "DANNON", "points": 1000, "timestamp": "2020-11-02T14:00:00Z"},
     {"payer": "UNILEVER", "points": 200, "timestamp": "2020-10-31T11:00:00Z"},
     {"payer": "DANNON", "points": -200, "timestamp": "2020-10-31T15:00:00Z"},
     {"payer": "MILLER COORS", "points": 10000,
@@ -8,17 +7,38 @@ TRANSACTIONS = [
     {"payer": "DANNON", "points": 300, "timestamp": "2020-10-31T10:00:00Z"}
 ]
 
-BALANCES = {}
+BALANCES = []
 
 
-def make_balances(transactions):
-    for transaction in transactions:
-        if transaction["payer"] in BALANCES:
-            BALANCES[transaction["payer"]]["points"] += transaction["points"]
-        else:
-            BALANCES[transaction["payer"]] = {
-                "payer": transaction["payer"], "points": transaction["points"]}
-        print(BALANCES)
+# def make_balances(transaction):
+#     for transaction in transactions:
+#         if transaction["payer"] in BALANCES:
+#             BALANCES[transaction["payer"]]["points"] += transaction["points"]
+#         else:
+#             BALANCES[transaction["payer"]] = {
+#                 "payer": transaction["payer"], "points": transaction["points"]}
+#     print(BALANCES)
+
+def make_balance(transaction):
+    print(BALANCES)
+    if len(BALANCES) == 0:
+        BALANCES.append(transaction)
+    else:
+        for balance in BALANCES:
+            if balance["payer"] == transaction["payer"]:
+                balance["points"] += transaction["points"]
+            else:
+                print("didnt exist")
+                BALANCES.append(transaction)
+    print(BALANCES)
+    return {"payer": transaction["payer"], "points": transaction["points"]}
 
 
-make_balances(TRANSACTIONS)
+# make_balances(TRANSACTIONS)
+print(make_balance({"payer": "DANNON", "points": 1000,
+                    "timestamp": "2020-11-02T14:00:00Z"}))
+print(make_balance({"payer": "DANNON", "points": 1000,
+                    "timestamp": "2020-11-02T14:00:00Z"}))
+print(make_balance(
+    {"payer": "UNILEVER", "points": 200, "timestamp": "2020-10-31T11:00:00Z"}))
+# print(BALANCES)
