0233044a98b73ed634767c455bfc210cec426dcb
Pierre Guilbault <guilbaultpj@gmail.com>
2021-11-18 11:17:38 +0100

    transaction and balances routes working

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from datetime import datetime, time
+from datetime import datetime, time, timezone
 from flask import Flask, request
 # from flask_restful import Resource, Api, reqparse
 app = Flask(__name__)
@@ -14,16 +14,31 @@ TRANSACTIONS = [
     {"payer": "DANNON", "points": 300, "timestamp": "2020-10-31T10:00:00Z"}
 ]
 
-
-def make_balances(transactions):
-    for transaction in transactions:
-        print(transaction)
-
-
-make_balances(TRANSACTIONS)
-
-
 BALANCES = []
+
+
+def make_balance(transaction):
+    if len(BALANCES) == 0:
+        print(
+            f'{transaction["payer"]} has been added to your Balances with {transaction["points"]} points.')
+        BALANCES.append(transaction)
+    else:
+        for balance in BALANCES:
+            if balance["payer"] == transaction["payer"]:
+                negativeCheck = balance["points"] + transaction["points"]
+                if negativeCheck < 0:
+                    print(
+                        f'Subtracting that amount will leave your points from {transaction["payer"]} in the negative. Unable to process.')
+                    # exit so that negative amount not shown.
+                    return
+                else:
+                    balance["points"] += transaction["points"]
+            else:
+                print(
+                    f'{transaction["payer"]} has been added to your Balances with {transaction["points"]} points.')
+                BALANCES.append(transaction)
+    return {"payer": transaction["payer"], "points": transaction["points"]}
+
 
 # if __name__ == "__main__":
 #     app.run(debug=True)
@@ -37,7 +52,7 @@ def balances():
     Returns all current balances
     """
     res = {}
-    for data_column in TRANSACTIONS:
+    for data_column in BALANCES:
         res[data_column["payer"]] = data_column["points"]
     return res
 
@@ -52,14 +67,19 @@ def catch_points():
     # return data["payer"], 201
     payer = data["payer"]
     points = data["points"]
-    timestamp = datetime.utcnow()
-    newTransaction = {"payer": payer, "points": points, "timestamp": timestamp}
+    utcDate = datetime.now(timezone.utc)
+    timestamp = utcDate.strftime("%Y-%m-%dT%H:%M:%SZ")
+    newTransaction = {"payer": payer, "points": points,
+                      "timestamp": timestamp}
     TRANSACTIONS.append(newTransaction)
+    make_balance(newTransaction)
+
     print(TRANSACTIONS)
+    print(BALANCES)
     return newTransaction, 201
 
 
-@app.route('/spend_points', methods=['POST'])
+@app.route('/spend_points', methods=['PUT'])
 def throw_points():
     """
     Spends points in total from each of the most recent transaction times until points are spent
