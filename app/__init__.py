9e1d9b0f8bfa41370f8cefc2fe00a8ec32cfd6dd
Pierre Guilbault <guilbaultpj@gmail.com>
2021-12-07 13:58:49 +0100

    spend-points route comments

--- app/__init__.py
+++ app/__init__.py
@@ -104,36 +104,66 @@ def throw_points():
     data = request.get_json()
     totalPointsToSpend = data["points"]
     pointsSpent = 0
+
+    # spread existing transactions into a new variable to not alter original order
     orderedTransactions = [*TRANSACTIONS]
+
+    # order transactions based on timestamp
     orderedTransactions.sort(
         key=lambda transaction: transaction['timestamp'])
+
+    # create a list to check for positive points remaining
     balancePoints = [BALANCES[key]["points"] for key in BALANCES]
+
+    # check that incoming points to spend does not exceed total points in balances
     if totalPointsToSpend > sum(balancePoints):
         return "Points to spend exceeds total points in Balances."
+
+    # check for transactions
     if len(TRANSACTIONS) == 0:
         return "There are no Transactions", 102
+
+    # check for points remaining in BALANCES
     elif all(points == 0 for points in balancePoints):
         return "All balances are at zero.", 403
+
     else:
+        # go through each transaction
         for transaction in orderedTransactions:
+
+            # if there are still points to spend and transaction points are not negative and not spent
             if totalPointsToSpend != pointsSpent and transaction["points"] >= 0 and transaction["points_spent"] < transaction["points"]:
 
                 pointsLeft = totalPointsToSpend - pointsSpent
+
+                # set pointsToSpend to the minimum
                 pointsToSpend = min(pointsLeft, transaction["points"])
 
+                # If the balance corresponding with the transactions payer is not zero and there transaction points have not been spent
                 if BALANCES[transaction["payer"]]["points"] != 0 and transaction["points_spent"] != transaction["points"]:
+
+                    # set points_spent to pointsToSpend if there are more or the same balance points than pointsToSpend
                     if pointsToSpend <= BALANCES[transaction["payer"]]["points"]:
                         transaction["points_spent"] += pointsToSpend
+                    # otherwise set points_spent to equal points
                     else:
                         transaction["points_spent"] = transaction["points"]
 
+                    # if subtracting the pointsToSpend from the balance will not make it negative
                     if (BALANCES[transaction["payer"]]["points"] - pointsToSpend) >= 0:
+                        # subtract those poinst
                         BALANCES[transaction["payer"]
                                  ]["points"] -= pointsToSpend
+                        # update our paintsSpent
                         pointsSpent += pointsToSpend
+
+                    # if it would make the balance negative
                     else:
                         balanceLeft = BALANCES[transaction["payer"]]["points"]
+                        # update our pointsSpent
                         pointsSpent += balanceLeft
+                        # and then make that balances points zero
                         BALANCES[transaction["payer"]]["points"] = 0
 
+    # return all balances in their current states
     return f"All points spent current balances are {BALANCES}", 201
